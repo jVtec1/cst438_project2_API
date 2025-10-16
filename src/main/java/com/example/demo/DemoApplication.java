bde4788f9aaf1933fac768393051557bf911f3bb
Andy Espinoza <andyesp831@gmail.com>
2025-10-16 13:00:52 -0700

    Added missing import for Principal java security class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.security.Principal;
+
 @SpringBootApplication
 @EnableWebSecurity
 @RestController
