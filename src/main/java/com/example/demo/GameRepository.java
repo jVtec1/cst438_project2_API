f5522d15afff45cc80c2565865b21554c7968d5e
SpIurge <sergonzalez@csumb.edu>
2025-10-22 21:33:03 -0700

    Adding Bet, Game, and Team Packages

--- src/main/java/com/example/demo/GameRepository.java
+++ src/main/java/com/example/demo/GameRepository.java
@@ -5,7 +5,7 @@ import com.example.demo.entity.Game.GameStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public interface GameRepository extends JpaRepository<Game,Long>{
     Page<Game> findByStatus(GameStatus status, Pageable pageable);
@@ -14,7 +14,4 @@ public interface GameRepository extends JpaRepository<Game,Long>{
 
     Page<Game> findByGameDateBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
 
-    Page<Game> findByHomeTeamIdAndStatus(Long homeTeamId, GameStatus status, Pageable pageable);
-
-    Page<Game> findByAwayTeamIdAndStatus(Long awayTeamId, GameStatus status, Pageable pageable);
 }
